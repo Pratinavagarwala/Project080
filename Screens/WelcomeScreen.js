e365ccc889d6c62dfc81fc2a66a77be6102cfb8b
Pratinav Agarwala <pratinavagarwa2006@gmail.com>
2021-01-28 22:33:46 +0530

    Project79

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as  React from "react" ;
-import {View,Text,TextInput,TouchableOpacity,StyleSheet} from "react-native";
+import {View,Text,TextInput,TouchableOpacity,StyleSheet,Alert,Modal,ScrollView,KeyboardAvoidingView} from "react-native";
 import firebase from "firebase";
 import db from "../config";
 
@@ -8,7 +8,13 @@ export default class WelcomeScreen extends React.Component{
         super();
         this.state={
             emailId:"",
-            password:""
+            password:"",
+            isModalVisible:false,
+            firstName:"",
+            lastName:"",
+            phoneNo:"",
+            address:"",
+            confirmPassword:""
         }
     }
     userLogin=(emailId,password)=>{
@@ -19,13 +25,142 @@ export default class WelcomeScreen extends React.Component{
             return Alert.alert(error.message)
         })
     }
+
+    userSignUp=(emailId,password,confirmPassword)=>{
+        if(password!==confirmPassword){
+           return Alert.alert("Password does not match")
+        }else{
+            firebase.auth().createUserWithEmailAndPassword(emailId,password)
+            .then(()=>{
+                 
+                db.collection("users").add({
+                    firstName:this.state.firstName,
+                    lastName:this.state.lastName,
+                    phoneNo:this.state.phoneNo,
+                    address:this.state.address,
+                    emailId:this.state.emailId,
+                })
+                return Alert.alert("New user registered")
+            }).catch((error)=>{
+                return Alert.alert(error.message)
+                
+            })
+        }
+    }
+    showModal=()=>{
+        return(
+            <Modal animationType="fade" transparent={false} visible={this.state.isModalVisible}>
+                <View style={{alignItems:"center"}}>
+                    <ScrollView style={{width:"100%",}}>
+                        <KeyboardAvoidingView style={{
+                            alignItems:"center"
+                        }}>
+                            <Text style={{marginTop:10}}>Registration</Text>
+                            <TextInput 
+                                placeholder="First Name"
+                                style={styles.inputBox}
+                                value={this.state.firstName}
+                                onChangeText={(text)=>{
+                                    this.setState({firstName:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                placeholder="Last Name"
+                                style={styles.inputBox}
+                                value={this.state.lastName}
+                                onChangeText={(text)=>{
+                                    this.setState({lastName:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                keyboardType="numeric"
+                                placeholder="Phone Number"
+                                style={styles.inputBox}
+                                value={this.state.phoneNo}
+                                onChangeText={(text)=>{
+                                    this.setState({phoneNo:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                placeholder="Address"
+                                style={styles.inputBox}
+                                value={this.state.address}
+                                onChangeText={(text)=>{
+                                    this.setState({address:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                style={styles.inputBox}
+                                placeholder="Email Id"
+                                keyboardType="email-address"
+                                onChangeText={(text)=>{
+                                    this.setState({
+                                        emailId:text
+                                    })
+                                }}
+                                value={
+                                    this.state.emailId
+                                }
+                            />
+
+                            <TextInput 
+                                style={styles.inputBox}
+                                placeholder="Password"
+                                secureTextEntry={true}
+                                onChangeText={(text)=>{
+                                    this.setState({
+                                        password:text
+                                    })
+                                }}
+                                value={
+                                    this.state.password
+                                }
+                            />
+
+                            <TextInput 
+                                style={styles.inputBox}
+                                placeholder="Confirm Password"
+                                secureTextEntry={true}
+                                onChangeText={(text)=>{
+                                    this.setState({
+                                        confirmPassword:text
+                                    })
+                                }}
+                                value={
+                                    this.state.confirmPassword
+                                }
+                            />
+
+                            <TouchableOpacity style={styles.button} onPress={()=>{
+                                this.userSignUp(this.state.emailId,this.state.password,this.state.confirmPassword)
+                            }}>
+                                <Text style={styles.text}>Register</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={styles.button} onPress={()=>{
+                                this.setState({isModalVisible:false})
+                            }}>
+                                <Text style={styles.text}>Cancel</Text>
+                            </TouchableOpacity>
+
+                        </KeyboardAvoidingView >
+                    </ScrollView>
+                </View>
+            </Modal>
+        )
+    }
     render(){
         return(
             <View style={styles.container}>
-                <Text style={styles.text}>Book Santa</Text>
+                {this.showModal()}
+                <Text style={styles.text}>Barter System</Text>
                 <TextInput 
                     style={styles.inputBox}
-                    placeholder="EmailId"
+                    placeholder="Email Id"
                     keyboardType="email-address"
                     onChangeText={(text)=>{
                         this.setState({
@@ -57,7 +192,9 @@ export default class WelcomeScreen extends React.Component{
                     <Text style={styles.text}>Login</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={()=>{
+                    this.setState({isModalVisible:true})
+                }}>
                     <Text style={styles.text}>Sign Up</Text>
                 </TouchableOpacity>
             </View>
@@ -70,11 +207,14 @@ const styles=StyleSheet.create(
        container:{
            alignItems:"center",
            justifyContent:"center",
-           marginTop:100,
+           marginTop:80,
        },
        text:{
            fontSize:20,
            fontWeight:"bold",
+           alignContent:"center",
+           alignSelf:"center",
+           justifyContent:"center"
            
        },
        inputBox:{
@@ -84,7 +224,9 @@ const styles=StyleSheet.create(
         padding:10,
         borderWidth:1,
         width:"80%",
-        height:100,
+        height:80,
+        marginLeft:"50%",
+        marginRight:"50%"
        },
        button:{
         alignItems:"center",
@@ -93,7 +235,7 @@ const styles=StyleSheet.create(
         padding:10,
         borderWidth:1,
         width:"50%",
-        height:100,
+        height:50,
        }
 
     }
